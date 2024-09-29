375039ee535ea69af1bb33f1b28fada8d3777d90
Yemmy <iiadeyemi307@gmail.com>
2024-09-29 20:46:24 +0100

    server.js up and running

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
+require('dotenv').config();
+
+app.use(express.json());
+
+const dbconfig = {
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+};
 
 
+const db = mysql.createConnection(dbconfig);
 
-const PORT = 3000;
+db.connect((err) => {
+    if (err) {
+        console.error("error connecting: ", err);
+        return;
+    }
+    console.log('connected to database sucessfully', db.threadId);
+
+    const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${3000}`)
-});
+
+
+    // sending a message to the server
+    app.get('/', (req, res) => {
+        res.send("Its TIME!!")
+    })
+    });
+})
+
+
+
